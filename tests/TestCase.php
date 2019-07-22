4d6fa868676bcbcdf89c965e30a677dac48fcdd6
Dmitriy <iankovd@gmail.com>
2019-07-22 18:07:17 +0200

    thumbnails tests added

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Iankov\Thumbnails\Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+
+abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../../../../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
